01427b1587a99b457a3c6e6638217871c63a83b9
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-10-01 14:48:24 +0100

    Create Prototype
    - Using the Grommet component library.
    - Adds an appbar, sidebar, main section, and controls section.
    - Using a static image for the data visualization.
    - Controls don't do anything yet.

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,25 +1,104 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import dataVisualization from "./ionosphere_mapTEC.jpeg";
+import { Box, Button, Heading, Grommet, Collapsible, Image } from "grommet";
+import { CirclePlay, Menu, Next, Previous } from "grommet-icons";
+
+const theme = {
+  global: {
+    font: {
+      family: "Roboto",
+      size: "18px",
+      height: "20px",
+    },
+  },
+};
 
 function App() {
+  const [showSidebar, setShowSidebar] = useState(true);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grommet theme={theme} full>
+        <Box fill>
+          <AppBar>
+            <Button
+              icon={<Menu />}
+              onClick={() => setShowSidebar(!showSidebar)}
+            />
+            <Heading
+              level="3"
+              margin={{
+                top: "none",
+                right: "none",
+                bottom: "none",
+                left: "1rem",
+              }}
+            >
+              The Fellowship of the Ionosphere
+            </Heading>
+          </AppBar>
+          <Box direction="row" flex overflow={{ horizontal: "hidden" }}>
+            <Sidebar open={showSidebar} />
+            <DataVisualization />
+          </Box>
+          <Controls />
+        </Box>
+      </Grommet>
     </div>
+  );
+}
+
+function AppBar(props: any) {
+  return (
+    <Box
+      tag="header"
+      direction="row"
+      align="center"
+      justify="start"
+      background="brand"
+      pad={{ left: "medium", right: "small", vertical: "small" }}
+      elevation="medium"
+      style={{ zIndex: "1" }}
+      {...props}
+    />
+  );
+}
+
+function Sidebar(props: any) {
+  return (
+    <Collapsible direction="horizontal" open={props.open}>
+      <Box
+        flex
+        width="medium"
+        background="light-2"
+        elevation="small"
+        align="center"
+        justify="center"
+      >
+        sidebar
+      </Box>
+    </Collapsible>
+  );
+}
+
+function DataVisualization(props: any) {
+  return (
+    <Box flex align="center" justify="center">
+      <Image fit="cover" src={dataVisualization} alt="An isonospheric map" />
+    </Box>
+  );
+}
+
+function Controls(props: any) {
+  return (
+    <Box align="center" justify="center" background="brand" pad="1rem">
+      <p>2022-09-28 18:10:00 UTC</p>
+      <Box direction="row">
+        <Button icon={<Previous />} />
+        <Button icon={<CirclePlay size="large" />} />
+        <Button icon={<Next />} />
+      </Box>
+    </Box>
   );
 }
 
