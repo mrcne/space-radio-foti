4529cfa50ae86f66f249d439588c64e5b08303ab
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-10-02 11:24:30 +0100

    Fetching hard-coded data

--- app/src/DataVisualization.tsx
+++ app/src/DataVisualization.tsx
@@ -5,82 +5,106 @@ import {
   GeoPermissibleObjects,
 } from "d3-geo";
 import { Box } from "grommet";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { feature } from "topojson";
+import { MultiPoint } from "geojson";
 
-export default function DataVisualization({
-  width = 960,
-  height = 1000,
-}: {
-  width?: number;
-  height?: number;
-}) {
+export default function DataVisualization({ size = 960 }: { size?: number }) {
+  const [points, setPoints] = useState<MultiPoint>();
+
   useEffect(() => {
-    renderVisualization();
-  });
+    getPoints().then((points) => setPoints(points));
+  }, []);
+
+  useEffect(() => {
+    if (points) renderVisualization(size, points);
+  }, [points, size]);
 
   return (
     <Box flex align="center" justify="center">
-      <canvas id="data-visualization" width={width} height={height}></canvas>
+      <canvas id="data-visualization" width={size} height={size}></canvas>
     </Box>
   );
+}
 
-  // https://observablehq.com/@d3/solar-terminator?collection=@d3/d3-geo
-  async function renderVisualization() {
-    const response = await fetch(
-      "https://cdn.jsdelivr.net/npm/world-atlas@2/land-50m.json"
-    );
-    const world = await response.json();
-    const land = feature(world, world.objects.land); // The land masses
-    const graticule = geoGraticule10(); // The grid of meridians and parallels
-    const sphere = { type: "Sphere" }; // An outline for the globe itself
+async function getPoints() {
+  // const response = await fetch(
+  //   "https://space-radio-foti.herokuapp.com/sample/spots"
+  // );
+  // const json = await response.json();
+  // return json;
 
-    const projection = geoNaturalEarth1();
+  const markers: MultiPoint = {
+    type: "MultiPoint",
+    coordinates: [
+      [100.0, 0.0],
+      [101.0, 1.0],
+    ],
+  };
 
-    const canvas = document.getElementById(
-      "data-visualization"
-    ) as HTMLCanvasElement;
+  return markers;
+}
 
-    if (!canvas) throw new Error("No canvas!");
+// https://observablehq.com/@d3/solar-terminator?collection=@d3/d3-geo
+async function renderVisualization(size: number, points: MultiPoint) {
+  const response = await fetch(
+    "https://cdn.jsdelivr.net/npm/world-atlas@2/land-50m.json"
+  );
+  const world = await response.json();
+  const land = feature(world, world.objects.land); // The land masses
+  const graticule = geoGraticule10(); // The grid of meridians and parallels
+  const sphere = { type: "Sphere" }; // An outline for the globe itself
 
-    var context = canvas.getContext("2d");
-    if (!context) throw new Error("No 2D context!");
+  const projection = geoNaturalEarth1();
 
-    fixPixellation(canvas, context, width);
+  const canvas = document.getElementById(
+    "data-visualization"
+  ) as HTMLCanvasElement;
 
-    // Hard-coding to center the drawing on the canvas 😭 there is definitely a smart way to do this...
-    context.translate(0, 200);
+  if (!canvas) throw new Error("No canvas!");
 
-    const path = geoPath(projection, context);
+  var context = canvas.getContext("2d");
+  if (!context) throw new Error("No 2D context!");
 
-    context.beginPath();
-    path(graticule);
-    context.strokeStyle = "#ccc";
-    context.stroke();
+  fixPixellation(canvas, context, size);
 
-    context.beginPath();
-    path(land);
-    context.fillStyle = "#000";
-    context.fill();
+  // Hard-coding to center the drawing on the canvas 😭 there is definitely a smart way to do this...
+  context.translate(0, 200);
 
-    context.beginPath();
-    path(sphere as GeoPermissibleObjects);
-    context.strokeStyle = "#000";
-    context.stroke();
-  }
+  const pathGenerator = geoPath(projection, context);
 
-  // https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio#correcting_resolution_in_a_canvas
-  function fixPixellation(canvas: any, context: any, size: number) {
-    // Set display size (css pixels).
-    canvas.style.width = size + "px";
-    canvas.style.height = size + "px";
+  context.beginPath();
+  pathGenerator(graticule);
+  context.strokeStyle = "#ccc";
+  context.stroke();
 
-    // Set actual size in memory (scaled to account for extra pixel density).
-    const scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
-    canvas.width = size * scale;
-    canvas.height = size * scale;
+  context.beginPath();
+  pathGenerator(land);
+  context.fillStyle = "#000";
+  context.fill();
 
-    // Normalize coordinate system to use css pixels.
-    context.scale(scale, scale);
-  }
+  context.beginPath();
+  pathGenerator(sphere as GeoPermissibleObjects);
+  context.strokeStyle = "#000";
+  context.stroke();
+
+  context.beginPath();
+  pathGenerator(points);
+  context.fillStyle = "#ee1b1b";
+  context.fill();
+}
+
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/devicePixelRatio#correcting_resolution_in_a_canvas
+function fixPixellation(canvas: any, context: any, size: number) {
+  // Set display size (css pixels).
+  canvas.style.width = size + "px";
+  canvas.style.height = size + "px";
+
+  // Set actual size in memory (scaled to account for extra pixel density).
+  const scale = window.devicePixelRatio; // Change to 1 on retina screens to see blurry canvas.
+  canvas.width = size * scale;
+  canvas.height = size * scale;
+
+  // Normalize coordinate system to use css pixels.
+  context.scale(scale, scale);
 }
