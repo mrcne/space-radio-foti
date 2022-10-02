7f17808ba80ab7c09c6474d2d24652c50bbea438
Daniel Metcalfe <mrdanielmetcalfe@gmail.com>
2022-10-02 09:21:37 +0100

    Add globe with D3-geo

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
-import dataVisualization from "./ionosphere_mapTEC.jpeg";
-import { Box, Button, Heading, Grommet, Collapsible, Image } from "grommet";
+/* eslint-disable @typescript-eslint/no-unused-expressions */
+import React, { useEffect, useState } from "react";
+import { Box, Button, Heading, Grommet, Collapsible } from "grommet";
 import { CirclePlay, Menu, Next, Previous } from "grommet-icons";
+import {
+  geoGraticule10,
+  geoNaturalEarth1,
+  geoPath,
+  GeoPermissibleObjects,
+} from "d3-geo";
+import { feature } from "topojson";
 
 const theme = {
   global: {
@@ -14,7 +21,7 @@ const theme = {
 };
 
 function App() {
-  const [showSidebar, setShowSidebar] = useState(true);
+  const [showSidebar, setShowSidebar] = useState(false);
 
   return (
     <div className="App">
@@ -81,10 +88,60 @@ function Sidebar(props: any) {
   );
 }
 
-function DataVisualization(props: any) {
+function DataVisualization({
+  width = 1000,
+  height = 500,
+}: {
+  width?: number;
+  height?: number;
+}) {
+  useEffect(() => {
+    // https://observablehq.com/@d3/solar-terminator?collection=@d3/d3-geo
+    async function renderVisualization() {
+      const response = await fetch(
+        "https://cdn.jsdelivr.net/npm/world-atlas@2/land-50m.json"
+      );
+      const world = await response.json();
+      const land = feature(world, world.objects.land); // The land masses
+      const graticule = geoGraticule10(); // The grid of meridians and parallels
+      const sphere = { type: "Sphere" }; // An outline for the globe itself
+
+      const projection = geoNaturalEarth1();
+      // projection.fitSize([width, height], land);
+
+      const canvas = document.getElementById(
+        "data-visualization"
+      ) as HTMLCanvasElement;
+
+      if (!canvas) throw new Error("No canvas!");
+
+      var context = canvas.getContext("2d");
+      if (!context) throw new Error("No 2D context!");
+
+      const path = geoPath(projection, context);
+
+      context.beginPath();
+      path(graticule);
+      context.strokeStyle = "#ccc";
+      context.stroke();
+
+      context.beginPath();
+      path(land);
+      context.fillStyle = "#000";
+      context.fill();
+
+      context.beginPath();
+      path(sphere as GeoPermissibleObjects);
+      context.strokeStyle = "#000";
+      context.stroke();
+    }
+
+    renderVisualization();
+  });
+
   return (
     <Box flex align="center" justify="center">
-      <Image fit="cover" src={dataVisualization} alt="An isonospheric map" />
+      <canvas id="data-visualization" width={width} height={height}></canvas>
     </Box>
   );
 }
